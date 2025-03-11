5500fc2f5f8244f7b0276083dbf489d3af1faa03
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2025-03-11 09:02:24 +0300

    setup or tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+ 
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE_URL || 'http://localhost:8001/api' || 'https://merry-go-round-six.vercel.app/api',
+    },
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css',
+        },
+      ],
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="text-center">
+        <h1 class="text-3xl font-bold underline">Home Page</h1>
+        <p className="font-bold" >Welcome to the home page</p>
+    </div>
+
+
+</template>
+<script>
+</script>
